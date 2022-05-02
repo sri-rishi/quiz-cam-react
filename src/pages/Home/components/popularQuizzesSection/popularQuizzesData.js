2da5d0c43870966db9d7e559b4ef1a0eeb01aa33
sri-rishi <sririshi2612@gmail.com>
2022-05-02 11:04:44 +0530

    chore: created data files

---
+++ src/pages/Home/components/popularQuizzesSection/popularQuizzesData.js
@@ -0,0 +1,25 @@
+import { wildlifeCover, ipaTrophy, dslrCameraCover } from "../../../../assets"
+
+export const popularQuizzesData = [
+    {
+        id: 1, 
+        quizName: "Wildlife",
+        quizCoverImage: wildlifeCover,
+        categoryName: "Photography",
+        lavel: 1
+    },
+    {
+        id: 2, 
+        quizName: "IPA",
+        quizCoverImage: ipaTrophy,
+        categoryName: "Awards",
+        lavel: 2,
+    },
+    {
+        id: 3, 
+        quizName: "DSLR",
+        quizCoverImage: dslrCameraCover,
+        categoryName: "Camera",
+        lavel: 2,
+    },
+]
