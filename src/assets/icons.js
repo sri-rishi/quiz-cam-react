d531033cd70265b638923da2bc32adc47613621a
sri-rishi <sririshi2612@gmail.com>
2022-05-02 11:03:58 +0530

    chore: imported icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,5 +1,13 @@
 import {GiHamburgerMenu} from "react-icons/gi";
+import {BsCodeSlash} from "react-icons/bs";
+import {AiOutlineGithub , AiFillLinkedin, AiOutlineTwitter} from "react-icons/ai";
+import {MdAlternateEmail} from "react-icons/md";
 
 export {
-    GiHamburgerMenu
+    GiHamburgerMenu,
+    BsCodeSlash,
+    AiOutlineGithub,
+    AiFillLinkedin,
+    AiOutlineTwitter,
+    MdAlternateEmail
 }
