f61c71fdc693b653f8891e4f40be8ddd6fa44a8b
sri-rishi <sririshi2612@gmail.com>
2022-05-04 23:02:22 +0530

    feat: added rules page

---
+++ src/pages/Rules/Rules.jsx
@@ -0,0 +1,28 @@
+import { Button, Navbar } from "../../component/index"
+
+export const RulesPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <main className="rule-main flex-column justify-center align-center">
+                <div className="rule-box card-shadow">
+                    <h3 className="xmd-heading center-align mb-1">Wildlife Photography</h3>
+                    <h4 className="sm-heading">Instructions</h4>
+                    <p className="xsm-heading">Read instructions carefully before playing quiz</p>
+                    <ul className="rule-list flex-column">
+                        <li>Each right answer scores 10 points</li>
+                        <li>Each wrong answer scores -10 points</li>
+                        <li>For each question you will get 1 minute to answer</li>
+                        <li>Each multiple choice question has one only one correct answer</li>
+                        <li>To win quiz you must score more than 65% </li>
+                    </ul>
+                    <div className="center-align mt-1">
+                        <a href="#">
+                            <Button className="btn btn-play-quiz" text="Start Quiz"/>
+                        </a>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
